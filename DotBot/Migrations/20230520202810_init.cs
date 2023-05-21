99ae61ad704dd49eaf050b4fe35b6d9d7f3def1f
dixxew <ayrat.volodin2@gmail.com>
2023-05-21 18:09:32 +0300

    applied migrations

---
+++ DotBot/Migrations/20230520202810_init.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nickname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marry = table.Column<int>(type: "int", nullable: false),
+                    MarryageRequest = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });;
+
+            migrationBuilder.CreateTable(
+                name: "GameStats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Exp = table.Column<int>(type: "int", nullable: false),
+                    ExpToUp = table.Column<int>(type: "int", nullable: false),
+                    Hp = table.Column<int>(type: "int", nullable: false),
+                    MaxHp = table.Column<int>(type: "int", nullable: false),
+                    Power = table.Column<int>(type: "int", nullable: false),
+                    Defence = table.Column<int>(type: "int", nullable: false),
+                    Kills = table.Column<int>(type: "int", nullable: false),
+                    DamageSum = table.Column<int>(type: "int", nullable: false),
+                    LevelPoints = table.Column<int>(type: "int", nullable: false),
+                    IsHealing = table.Column<bool>(type: "bit", nullable: false),
+                    Money = table.Column<int>(type: "int", nullable: false),
+                    Armorid = table.Column<int>(type: "int", nullable: false),
+                    Weaponid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameStats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameStats_Armors_Armorid",
+                        column: x => x.Armorid,
+                        principalTable: "Armors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameStats_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameStats_Weapons_Weaponid",
+                        column: x => x.Weaponid,
+                        principalTable: "Weapons",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStats_Armorid",
+                table: "GameStats",
+                column: "Armorid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStats_UserId",
+                table: "GameStats",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStats_Weaponid",
+                table: "GameStats",
+                column: "Weaponid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameStats");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
