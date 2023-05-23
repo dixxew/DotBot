a8a959b6af4cab1704e775fe9d3634b6497fd7e8
dixxew <ayrat.volodin2@gmail.com>
2023-05-23 15:13:30 +0300

    rating controller prapared for js front

--- DotBot/Controllers/RatingController.cs
+++ DotBot/Controllers/RatingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using DotBot.Models;
 using DotBot.Data;
@@ -9,6 +10,11 @@ using VkNet.Model.Attachments;
 using VkNet.Model;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using DotBot.DAL;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore.Query;
+using Newtonsoft.Json;
+using Object = System.Object;
 using User = DotBot.Models.User;
 
 namespace DotBot.Controllers
@@ -16,66 +22,44 @@ namespace DotBot.Controllers
     public class RatingController : Controller
     {
 
-        private readonly IConfiguration _configuration;
-        VkApi api = new VkApi();
-        private readonly vkContext db;
-       
-        public RatingController(IConfiguration configuration, vkContext context)
+        private DbRepository db = new DbRepository();       
+        public RatingController(IConfiguration configuration)
         {
-            _configuration = configuration;
-            db = context;
-            if (!api.IsAuthorized)
-            {
-                api.Authorize(new ApiAuthParams
-                {
-                    AccessToken = _configuration["Config:AccessToken"]
-                });
-            }
+           
         }
 
         public IActionResult Index()
         {
-         List<GameStat> fromGsData;
-        List<Rating> usersModel = new List<Rating>();
-        Rating item = new Rating();
-         List<Weapon> weapDat;
-         List<Armor> armDat;
-         List<User> fromUsersData;
-        weapDat = db.Weapons.ToList();
-            armDat = db.Armors.ToList();
-            fromGsData = db.GameStats.OrderByDescending(x => x.exp).Take(50).ToList();
-            int[] ids = fromGsData.Select(x => x.Id).ToArray();
-            fromUsersData = db.Users.Where(z => ids.Contains(z.Id)).ToList();
-            foreach (int id in ids)
-            {
-                var gsItem = fromGsData.FirstOrDefault(x => x.Id == id);
-                item = new Rating();
-                item.itemStrength = armDat.FirstOrDefault(x => x.id == gsItem.armorId).protect;
-                item.itemStrength += weapDat.FirstOrDefault(x => x.id == gsItem.weaponId).damage;
-                if (fromUsersData.Find(x => x.Id == gsItem.Id).Marry > 0)
-                {
-                    item.IsMarriage = true;
-                }
-                item.Name = fromUsersData.FirstOrDefault(x => x.Id == gsItem.Id).Nickname;
-                item.photoUrl = api.Users.Get(new long[] { gsItem.Id }, VkNet.Enums.Filters.ProfileFields.Photo50).FirstOrDefault().Photo50;
-                item.Lvl = gsItem.lvl;
-                item.Id = gsItem.Id;
-                item.Strength = gsItem.defence + gsItem.power;
-                item.profileUrl = "https://vk.com/id" + gsItem.Id;
-                usersModel.Add(item);
-            }
-            return View(usersModel);
+         
+            return View();
         }
-
-        
+        [HttpGet]
+        [EnableCors]
+        public Object data()
+        {
+            List<Rating>  data = new List<Rating>();
+            List<GameStat> dataSet = db.GameStatRepository.Get(null,null,"User,Weapon,Armor").OrderByDescending(x => x.Exp).Take(25).ToList();
+            int count = dataSet.Count;
+            foreach (var item in dataSet)
+            {
+                Rating rating = new Rating();
+                bool isMarry = item.User.Marry > 0;
+                rating.Id = item.Id;
+                rating.IsMarriage = isMarry;
+                rating.Lvl = item.Level;
+                rating.Name = item.User.Name;
+                rating.Strength = item.Power = item.Defence;
+                rating.itemStrength = item.Weapon.damage + item.Armor.protect;
+                data.Add(rating);
+            }
+            return JsonConvert.SerializeObject(data);
+        }
 
     }
 
     public class Rating
     {
         public int Id { get; set; }
-        public string profileUrl { get; set; }
-        public Uri photoUrl { get; set; }
         public string Name { get; set; }
         public int Lvl { get; set; }
         public bool IsMarriage { get; set; } = false;
